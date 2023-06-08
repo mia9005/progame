72aa1bc74b6bd32b504af5fea47638bb64c2ab84
Jeongeun Lee <jungeun9005@gmail.com>
2023-06-08 16:13:07 -0700

    change folder

--- Logout.php
+++ Logout.php
@@ -2,7 +2,7 @@
 require_once("inc/config.inc.php");
 require_once("inc/Entities/Customer.class.php");
 require_once("inc/Utilities/PDOService.class.php");
-require_once("inc/Utilities/DAO/CustomerDAO.class.php");
+require_once("inc/DAO/CustomerDAO.class.php");
 require_once("inc/Utilities/Page.class.php");
 require_once("inc/Utilities/LoginManager.class.php");
 require_once("Login.php");
